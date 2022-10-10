74e07fa43b6cff273e71bcd80103e2670d311c2c
sribalajidev <developer.sribalaji@gmail.com>
2022-10-10 11:26:18 +0530

    Project - FE updates

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,9 +1,9 @@
 
 function SearchBar() {
   return (
-    <form action="" role="search" class="search-form">
-		<input type="search" name="search-text" class="search-text" placeholder="Search..." autocomplete="off" />
-		<input type="submit" value="" class="search-submit" />
+    <form action="" role="search" className="search-form">
+		<input type="search" name="search-text" className="search-text" placeholder="Search..." autoComplete="off" />
+		<input type="submit" value="" className="search-submit" />
   </form>
   );
 }
